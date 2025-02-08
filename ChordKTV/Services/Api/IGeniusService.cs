71d1a32f9bbf4a56a15ff495d066376e042f12b9
Haddy Alchaer <halchaer@purdue.edu>
2025-02-08 18:40:36 -0500

    string sinetad of model types

--- ChordKTV/Services/Api/IGeniusService.cs
+++ ChordKTV/Services/Api/IGeniusService.cs
@@ -1,10 +1,11 @@
 namespace ChordKTV.Services.Api;
 
 using ChordKTV.Dtos;
+using ChordKTV.Models.SongData;
 
 public interface IGeniusService
 {
-    Task<Models.SongData.Song?> GetSongByArtistTitleAsync(string title, string? artist);
-    Task<List<Models.SongData.Song>> GetSongsByArtistTitleAsync(List<VideoInfo> videos);
-    Task<Models.SongData.Song?> EnrichSongDetailsAsync(Models.SongData.Song song);
+    Task<Song?> GetSongByArtistTitleAsync(string title, string? artist);
+    Task<List<Song>> GetSongsByArtistTitleAsync(List<VideoInfo> videos);
+    Task<Song?> EnrichSongDetailsAsync(Song song);
 }
