b5604fc80e1a7ef725756c078f461cd800d9c8c2
Haddy Alchaer <halchaer@purdue.edu>
2025-02-16 12:41:59 -0500

    lint check

--- frontend/lyric-sync-app/src/components/AppBarComponent.tsx
+++ frontend/lyric-sync-app/src/components/AppBarComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button, Avatar, Menu, MenuItem } from '@mui/material';
+import { AppBar, Toolbar, Typography, Avatar, Menu, MenuItem } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { GoogleLogin } from '@react-oauth/google';
 import { useAuth } from '../contexts/AuthContext';
