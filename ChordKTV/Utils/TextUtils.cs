97a343875ff94b756773c7f9590d1e909306325e
Darin Tsai <darint916@gmail.com>
2025-02-19 17:07:29 -0500

    romanization expansion pack

--- ChordKTV/Utils/TextUtils.cs
+++ ChordKTV/Utils/TextUtils.cs
@@ -15,6 +15,10 @@ public static class LanguageUtils
     public static bool IsLatinCharacter(char c) => c switch
     {
         >= '\u0000' and <= '\u024F' => true, // Latin scripts: ASCII, Latin-1, Extended-A & B
+        >= '\u1E00' and <= '\u1EFF' => true, //Latin Extended Additional
+        >= '\u02B0' and <= '\u02FF' => true, //assume these are special spacing chars
+        >= '\u0300' and <= '\u036F' => true, //combining marks
+        //assume further is greek
         _ => false
     };
 }
