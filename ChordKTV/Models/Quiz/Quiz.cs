b1032cd7234832e8d9bb807a5f2d3e793cd84242
Haddy Alchaer <halchaer@purdue.edu>
2025-03-01 11:35:38 -0500

    quiz guid consistent and not weirdly replaced

--- ChordKTV/Models/Quiz/Quiz.cs
+++ ChordKTV/Models/Quiz/Quiz.cs
@@ -4,7 +4,7 @@ namespace ChordKTV.Models.Quiz;
 
 public sealed class Quiz
 {
-    public global::System.Guid Id { get; set; } = global::System.Guid.NewGuid();
+    public global::System.Guid Id { get; set; }
     public global::System.Guid SongId { get; set; }
     public global::System.Collections.Generic.List<QuizQuestion> Questions { get; set; } = [];
     public global::System.DateTime Timestamp { get; set; } = global::System.DateTime.UtcNow;
