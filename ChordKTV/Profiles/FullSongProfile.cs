e77f1ae4a5c095fd6427a2846879dcde59bbbc36
Darin Tsai <darint916@gmail.com>
2025-03-14 14:33:07 -0400

    record profile map fix

--- ChordKTV/Profiles/FullSongProfile.cs
+++ ChordKTV/Profiles/FullSongProfile.cs
@@ -9,6 +9,6 @@ public class FullSongProfile : Profile
     public FullSongProfile()
     {
         CreateMap<Song, FullSongResponseDto>()
-            .ForMember("AlbumNames", opt => opt.MapFrom(src => src.Albums.Select(a => a.Name).ToList()));
+            .ForCtorParam("AlbumNames", opt => opt.MapFrom(src => src.Albums.Select(a => a.Name).ToList()));
     }
 }
