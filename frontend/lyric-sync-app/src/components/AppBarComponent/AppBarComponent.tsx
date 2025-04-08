60125c714ac7c0c5824319f6a0f5fb6ac2b38e36
Darin Tsai <darint916@gmail.com>
2025-04-08 17:25:57 -0400

    template init

--- frontend/lyric-sync-app/src/components/AppBarComponent/AppBarComponent.tsx
+++ frontend/lyric-sync-app/src/components/AppBarComponent/AppBarComponent.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { AppBar, Toolbar, Avatar, Menu, MenuItem } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Avatar, Menu, MenuItem, Button, Dialog, DialogTitle, DialogContent, DialogActions, TextField, Box, Typography, IconButton } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthTypes';
+import CloseIcon from '@mui/icons-material/Close';
+import BugReportIcon from '@mui/icons-material/BugReport';
 import GoogleAuth from '../GoogleAuth/GoogleAuth';
 import './AppBarComponent.scss';
 import logo from '../../assets/chordktv.png';
@@ -9,6 +11,13 @@ import logo from '../../assets/chordktv.png';
 const AppBarComponent: React.FC = () => {
   const { user, setUser } = useAuth();
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [bugMenuAnchorEl, setBugMenuAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [issueType, setIssueType] = useState('');
+  const [issueTitle, setIssueTitle] = useState('');
+
+  const repoOwner = 'darint916';
+  const repoName = 'chordKTV';
 
   const handleLogout = () => {
     setUser(null);
@@ -24,39 +33,159 @@ const AppBarComponent: React.FC = () => {
     setAnchorEl(null);
   };
 
+  const handleBugMenuClick = (event: React.MouseEvent<HTMLElement>) => {
+    setBugMenuAnchorEl(event.currentTarget);
+  };
+
+  const handleBugMenuClose = () => {
+    setBugMenuAnchorEl(null);
+  };
+
+  const openIssueModal = (type: string) => {
+    setIssueType(type);
+    setModalOpen(true);
+    handleBugMenuClose();
+  };
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
+
+  const handleOpenGitHubIssue = () => {
+    // Base URL for GitHub issue creation
+    const baseUrl = `https://github.com/${repoOwner}/${repoName}/issues/new`;
+
+    // Get the appropriate template for the issue type
+    let template = '';
+    let labels = '';
+
+    switch (issueType) {
+    case 'bug':
+      template = 'bug_report.md';
+      labels = 'bug';
+      break;
+    case 'feature':
+      template = 'feature_request.md';
+      labels = 'enhancement';
+      break;
+    case 'lyrics':
+      template = 'lyrics_issue.md';
+      labels = 'lyrics,content';
+      break;
+    default:
+      template = '';
+    }
+
+    const encodedTitle = encodeURIComponent(issueTitle || `New ${issueType} report`);
+
+    let url = `${baseUrl}?`;
+    if (template) {
+      url += `template=${template}&`;
+    }
+    url += `title=${encodedTitle}&labels=${labels}`;
+    window.open(url, '_blank');
+    setModalOpen(false);
+    setIssueTitle('');
+  };
+
   return (
-    <AppBar position="static" elevation={0} className="app-bar">
-      <Toolbar className="toolbar">
-        <div className="section">
-          <Link to="/"><img src={logo} alt="Logo" className="logo" /></Link>
-          <Link to="/" className="title">
-            Chord KTV
-          </Link>
-        </div>
-        <div className="section">
-          {user ? (
-            <>
-              <Avatar
-                src={user.picture}
-                alt={user.name}
-                onClick={handleMenuClick}
-                className="avatar"
-              />
-              <Menu
-                anchorEl={anchorEl}
-                open={Boolean(anchorEl)}
-                onClose={handleMenuClose}
-              >
-                <MenuItem>{user.name}</MenuItem>
-                <MenuItem onClick={handleLogout}>Logout</MenuItem>
-              </Menu>
-            </>
-          ) : (
-            <GoogleAuth />
-          )}
-        </div>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="static" elevation={0} className="app-bar">
+        <Toolbar className="toolbar">
+          <div className="section">
+            <Link to="/"><img src={logo} alt="Logo" className="logo" /></Link>
+            <Link to="/" className="title">
+              Chord KTV
+            </Link>
+          </div>
+          <div className="section">
+            <Button
+              color="inherit"
+              startIcon={<BugReportIcon />}
+              onClick={handleBugMenuClick}
+              sx={{ marginRight: 2 }}
+            >
+              Report Issue
+            </Button>
+            <Menu
+              anchorEl={bugMenuAnchorEl}
+              open={Boolean(bugMenuAnchorEl)}
+              onClose={handleBugMenuClose}
+            >
+              <MenuItem onClick={() => openIssueModal('bug')}>Report Bug</MenuItem>
+              <MenuItem onClick={() => openIssueModal('feature')}>Request Feature</MenuItem>
+              <MenuItem onClick={() => openIssueModal('lyrics')}>Report Lyrics Issue</MenuItem>
+            </Menu>
+
+            {user ? (
+              <>
+                <Avatar
+                  src={user.picture}
+                  alt={user.name}
+                  onClick={handleMenuClick}
+                  className="avatar"
+                />
+                <Menu
+                  anchorEl={anchorEl}
+                  open={Boolean(anchorEl)}
+                  onClose={handleMenuClose}
+                >
+                  <MenuItem>{user.name}</MenuItem>
+                  <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                </Menu>
+              </>
+            ) : (
+              <GoogleAuth />
+            )}
+          </div>
+        </Toolbar>
+      </AppBar>
+
+      <Dialog
+        open={modalOpen}
+        onClose={handleModalClose}
+        maxWidth="sm"
+        fullWidth
+      >
+        <DialogTitle>
+          <Box display="flex" justifyContent="space-between" alignItems="center">
+            <Typography variant="h6">
+              {issueType === 'bug' ? 'Report a Bug' :
+                issueType === 'feature' ? 'Request a Feature' :
+                  'Report Lyrics Issue'}
+            </Typography>
+            <IconButton onClick={handleModalClose}>
+              <CloseIcon />
+            </IconButton>
+          </Box>
+        </DialogTitle>
+        <DialogContent>
+          <Typography variant="body2" sx={{ mb: 2 }}>
+            You'll be redirected to GitHub to create an issue with the appropriate template.
+            Please enter a brief title for your issue:
+          </Typography>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Issue Title"
+            fullWidth
+            value={issueTitle}
+            onChange={(e) => setIssueTitle(e.target.value)}
+            variant="outlined"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleModalClose}>Cancel</Button>
+          <Button
+            onClick={handleOpenGitHubIssue}
+            variant="contained"
+            color="primary"
+          >
+            Continue to GitHub
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
