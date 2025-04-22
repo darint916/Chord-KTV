9c7d80a3fa334b9acaf106e36d4e276af8f549fa
Parthiv Shaji <parthiv6@gmail.com>
2025-04-21 20:04:03 -0400

    Regenerate OpenAPI stubs

--- frontend/lyric-sync-app/src/api/apis/index.ts
+++ frontend/lyric-sync-app/src/api/apis/index.ts
@@ -1,7 +1,6 @@
 /* tslint:disable */
- 
+/* eslint-disable */
 export * from './HandwritingApi';
 export * from './QuizApi';
 export * from './SongApi';
 export * from './UserApi';
-export * from './AuthApi';
