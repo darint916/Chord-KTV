9e7a6ede5521b680a27d37e6aeb1adb6017a1346
Parthiv Shaji <parthiv6@gmail.com>
2025-04-24 19:52:46 -0400

    Regenerate stubs

--- frontend/lyric-sync-app/src/api/models/index.ts
+++ frontend/lyric-sync-app/src/api/models/index.ts
@@ -1,5 +1,5 @@
 /* tslint:disable */
- 
+/* eslint-disable */
 export * from './Album';
 export * from './FullSongRequestDto';
 export * from './FullSongResponseDto';
@@ -8,6 +8,7 @@ export * from './GeniusMetaDataDto';
 export * from './HandwritingCanvasRequestDto';
 export * from './HandwritingCanvasResponseDto';
 export * from './LanguageCode';
+export * from './LearnedWordDto';
 export * from './MatchScores';
 export * from './PlaylistDetailsDto';
 export * from './ProblemDetails';
@@ -15,4 +16,8 @@ export * from './QuizQuestionDto';
 export * from './QuizResponseDto';
 export * from './Song';
 export * from './TranslationRequestDto';
+export * from './UserHandwritingResultDto';
+export * from './UserPlaylistActivityDto';
+export * from './UserQuizResultDto';
+export * from './UserSongActivityDto';
 export * from './VideoInfo';
